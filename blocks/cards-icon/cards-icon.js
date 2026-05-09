42bb99ee9e6893a8d66a9ac56cc1778324b43d3e
timwlee <timothyl@adobe.com>
2026-05-08 20:38:53 -0700

    WIP

--- blocks/cards-icon/cards-icon.js
+++ blocks/cards-icon/cards-icon.js
@@ -1,21 +1,52 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement('ul');
+  const introDiv = document.createElement('div');
+  introDiv.className = 'cards-icon-intro';
+
+  const iconsRow = document.createElement('ul');
+  iconsRow.className = 'cards-icon-icons-row';
+
   [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-icon-card-image';
-      else div.className = 'cards-icon-card-body';
-    });
-    ul.append(li);
+    const hasImage = row.querySelector('picture');
+
+    if (!hasImage) {
+      // Intro text card
+      while (row.firstElementChild) introDiv.append(row.firstElementChild);
+    } else {
+      // Icon card
+      const li = document.createElement('li');
+      [...row.children].forEach((div) => {
+        if (div.children.length === 1 && div.querySelector('picture')) {
+          div.className = 'cards-icon-card-image';
+        } else {
+          div.className = 'cards-icon-card-body';
+        }
+        li.append(div);
+      });
+      iconsRow.append(li);
+    }
   });
-  ul.querySelectorAll('picture > img').forEach((img) => {
+
+  iconsRow.querySelectorAll('picture > img').forEach((img) => {
     const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
     img.closest('picture').replaceWith(optimizedPic);
   });
+
   block.textContent = '';
-  block.append(ul);
+  if (introDiv.hasChildNodes()) block.append(introDiv);
+  if (iconsRow.hasChildNodes()) block.append(iconsRow);
+
+  block.querySelectorAll('a[href]').forEach((a) => {
+    a.setAttribute('target', '_blank');
+    a.setAttribute('rel', 'noopener noreferrer');
+
+    const icon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    icon.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+    icon.setAttribute('viewBox', '0 0 16 16');
+    icon.setAttribute('aria-hidden', 'true');
+    icon.classList.add('cards-icon-external-icon');
+    icon.innerHTML = '<path d="M13 9v4a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h4V1H3a3 3 0 0 0-3 3v9a3 3 0 0 0 3 3h9a3 3 0 0 0 3-3V9h-2zm-3-8v2h2.586L6.293 9.293l1.414 1.414L14 4.414V7h2V1h-6z"/>';
+    a.append(icon);
+  });
 }
