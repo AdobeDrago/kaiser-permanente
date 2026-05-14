95c05cc291408cce3f3802b3db4fce8625337f1e
timwlee <timothyl@adobe.com>
2026-05-13 22:32:56 -0700

    WIP

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -15,11 +15,32 @@ export default async function decorate(block) {
   const thead = document.createElement('thead');
   const tbody = document.createElement('tbody');
 
+  const allRows = [...block.children];
+  const firstRowCols = [...allRows[0].children];
+  const hasSingleContentRow = allRows.length > 1
+    && firstRowCols[0].textContent.trim() !== ''
+    && firstRowCols.slice(1).every((col) => col.textContent.trim() === '');
+
+  const isCaption = block.classList.contains('caption');
+  const hasCaption = isCaption && hasSingleContentRow;
+  const hasTitle = !isCaption && hasSingleContentRow;
+  const rows = (hasCaption || hasTitle) ? allRows.slice(1) : allRows;
+
   const header = !block.classList.contains('no-header');
   if (header) table.append(thead);
   table.append(tbody);
 
-  [...block.children].forEach((child, i) => {
+  if (hasCaption) {
+    const numCols = [...allRows[1].children].length;
+    const captionRow = document.createElement('tr');
+    const captionCell = document.createElement('th');
+    captionCell.setAttribute('colspan', numCols);
+    captionCell.innerHTML = firstRowCols[0].innerHTML;
+    captionRow.append(captionCell);
+    thead.append(captionRow);
+  }
+
+  rows.forEach((child, i) => {
     const row = document.createElement('tr');
     if (header && i === 0) thead.append(row);
     else tbody.append(row);
@@ -29,6 +50,15 @@ export default async function decorate(block) {
       row.append(cell);
     });
   });
+
   block.innerHTML = '';
+
+  if (hasTitle) {
+    const titleEl = document.createElement('div');
+    titleEl.className = 'table-title';
+    titleEl.innerHTML = allRows[0].children[0].innerHTML;
+    block.append(titleEl);
+  }
+
   block.append(table);
 }
